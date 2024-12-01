70137e64bc78943021022acb74044c9be9f73db3
DarynaBb <darinabilenko@gmail.com>
2024-12-01 20:03:20 +0100

    add main func + prototypes of help functions

---
+++ print_f/ft_printf.c
@@ -0,0 +1,48 @@
+#include "ft_printf.h"
+
+static int var_format(char spec, va_list *args)
+{
+	if (spec == 'c')
+		return (ft_print_char(va_arg(*args, int)));
+	if (spec == 's')
+		return (ft_print_str(va_arg(*args, const char *)));
+	if (spec == 'p')
+		return (ft_print_pointer(va_arg(*args, void *)));
+	if (spec == 'd' || spec == 'i')
+		return (ft_print_int(va_arg(*args, int)));
+	if (spec == 'u')
+		return (ft_print_unsigned_int(va_arg(*args, unsigned int)));
+	if (spec == 'x' || spec == 'X')
+		return (ft_print_hex(va_arg(*args, unsigned int)));
+	if (spec == '%')
+		return (write(1, "%", 1));
+	return (0);
+	}
+
+int ft_printf(const char *str, ...)
+{
+	va_list	args;
+	va_start(args, str);
+	int	count;
+	int	sum;
+
+	count = 0;
+	sum = 0;
+	while (*str)
+	{
+		if (*str == '%' && *(str + 1))
+		{
+			str++;
+			count += handle_spec(*str, &args);
+		} else 
+		{
+			write (1, str, 1);
+			count++;
+		}
+		if (count < 0)
+			return (-1);
+		sum += count;	
+	}
+	va_end(args);
+	return (sum);
+}
---
+++ print_f/ft_printf.h
@@ -0,0 +1,13 @@
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <limits.h>
+# include <stdarg.h>
+
+int	ft_print_char(char c);
+
+
+#endif
