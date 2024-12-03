d9739f81afb2d5289c9e38b6ab5c4a8f6ada09ce
DarynaBb <darinabilenko@gmail.com>
2024-12-03 14:29:14 +0100

    add print_pointer func

--- print_f/ft_printf.h
+++ print_f/ft_printf.h
@@ -4,8 +4,8 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <stdio.h>
-# include <limits.h>
 # include <stdarg.h>
+# include <stdint.h>
 
 int	print_char(char c);
 int	print_str(const char *str);
@@ -13,5 +13,10 @@ int	print_int(int num);
 int	print_unsigned_int(unsigned int num);
 int	print_hex(unsigned int num, char spec);
 int	print_pointer(uintptr_t ptr);
+
+// Utils
+char	*dec_to_hex(unsigned int num, char spec);
+char	*hex_string(char *str, int length);
+char	*handle_zero_case(void);
 
 #endif
--- print_f/print_pointer.c
+++ print_f/print_pointer.c
@@ -2,5 +2,27 @@
 
 int	print_pointer(uintptr_t ptr)
 {
-	
+	char	*str;
+	int		count;
+	int		str_count;
+
+	if (!ptr)
+	{
+		return (write(1, "(nil)", 5));
+	}
+	count = write(1, "0x", 2);
+	if (count < 0)
+		return (-1);
+	str = dec_to_hex(ptr, 'x');
+	if (!str)
+		return (-1);
+	str_count = print_str(str);
+	if (str_count < 0)
+	{
+		free(str);
+		return (-1);
+	}
+	count += str_count;
+	free(str);
+	return (count);
 }
