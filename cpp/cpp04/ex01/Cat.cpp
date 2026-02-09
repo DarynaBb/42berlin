22d87154e4f522e7b1d904a5b83df305aaa75898
DarynaBb <darinabilenko@gmail.com>
2026-02-09 16:17:05 +0100

    ex01 added

---
+++ cpp/cpp04/ex01/Cat.cpp
@@ -0,0 +1,37 @@
+#include "Cat.hpp"
+#include <iostream>
+
+Cat::Cat() : Animal()
+{
+	this->type = "Cat";
+	this->brain = new Brain();
+	std::cout << "Cat default constructor called" << std::endl;
+}
+
+Cat::Cat(const Cat& src) : Animal(src)
+{
+	this->brain = new Brain(*src.brain);
+	std::cout << "Cat copy constructor called" << std::endl;
+}
+Cat& Cat::operator=(const Cat& src)
+{
+	std::cout << "Cat copy assignment operator called" << std::endl;
+	if (this != &src)
+	{
+		Animal::operator=(src);
+		delete this->brain;
+		this->brain = new Brain(*src.brain);
+	}
+	return *this;
+}
+
+Cat::~Cat()
+{
+	delete this->brain;
+	std::cout << "Cat destructor called" << std::endl;
+}
+
+void Cat::makeSound() const
+{
+	std::cout << "Miau! Miau!" << std::endl;
+}
